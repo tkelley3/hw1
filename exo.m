00349fb1bd1582b4d19ce5daeb125f6987a54719
Tom Kelley <th.kelley@northeastern.edu>
2022-09-21 20:07:15 -0400

    Update exo.m

--- exo.m
+++ exo.m
@@ -1,4 +1,2 @@
-% Reads data into a Table
-planets = readtable("exoplanetsclean.csv");
-% You can reference an individual column using the dot (.) notation
-planets.Name(1:10)
+% File name
+exopl = "exoplanet2022cleaned.csv"
